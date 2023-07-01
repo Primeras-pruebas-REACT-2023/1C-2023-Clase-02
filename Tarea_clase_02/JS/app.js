7cb512c8199f3451817eeb872722a1456b86b945
Walter Mancuso <waltermancuso@abc.gob.ar>
2023-07-01 11:28:30 -0300

    Actualizacion 01/07/2023

--- Tarea_clase_02/JS/app.js
+++ Tarea_clase_02/JS/app.js
@@ -2,5 +2,6 @@ var a = 100;
 var b = 200;
 console.log(a+b);
 
-var nombre = prompt ("¿Como te llamas?");
-alert("Hola", nombre);
+var prueba = prompt ("¿Como te llamas?");
+
+alert("Hola " + prueba);
